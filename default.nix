3a027580a2e4223adc42833ef62175f6d727f480
reesilva <renato.biancalana@taxfix.de>
2025-10-18 13:33:30 +0200

    fix: temporary use gtk2
    until issue solved for building gtk3 in darwin

--- default.nix
+++ default.nix
@@ -1,23 +1,31 @@
 {
-  pkgs ? import <nixpkgs> {},
+  pkgs ? import <nixpkgs> { },
   system ? pkgs.stdenv.hostPlatform.system,
-}: let
-  mkZen = name: entry: let
-    variant = (builtins.fromJSON (builtins.readFile ./sources.json)).${entry}.${system};
-  in
+}:
+let
+  mkZen =
+    name: entry:
+    let
+      variant = (builtins.fromJSON (builtins.readFile ./sources.json)).${entry}.${system};
+    in
     pkgs.callPackage ./package.nix {
       inherit name variant;
     };
-in rec {
+in
+rec {
   beta-unwrapped = mkZen "beta" "beta";
   twilight-unwrapped = mkZen "twilight" "twilight";
   twilight-official-unwrapped = mkZen "twilight" "twilight-official";
 
-  beta = pkgs.wrapFirefox beta-unwrapped {
+  beta = pkgs.wrapFirefox.override { libcanberra-gtk3 = pkgs.libcanberra-gtk2; } beta-unwrapped {
     icon = "zen-browser";
   };
-  twilight = pkgs.wrapFirefox twilight-unwrapped {};
-  twilight-official = pkgs.wrapFirefox twilight-official-unwrapped {};
+  twilight = pkgs.wrapFirefox.override {
+    libcanberra-gtk3 = pkgs.libcanberra-gtk2;
+  } twilight-unwrapped { };
+  twilight-official = pkgs.wrapFirefox.override {
+    libcanberra-gtk3 = pkgs.libcanberra-gtk2;
+  } twilight-official-unwrapped { };
 
   default = beta;
 }
